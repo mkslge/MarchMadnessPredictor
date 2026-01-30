64bfa56668ceb5898f376b04bef486dbd74f144d
mkslge <marklseeliger@gmail.com>
2026-01-30 07:57:43 -0500

    init

---
+++ src/main/java/org/example/marchmadness/MarchMadnessApplication.java
@@ -0,0 +1,13 @@
+package org.example.marchmadness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarchMadnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarchMadnessApplication.class, args);
+	}
+
+}
